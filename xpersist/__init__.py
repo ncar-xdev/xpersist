0dc18d487bac31889a52eea569f1f37c470bfef1
Anderson Banihirwe <axbanihirwe@ualr.edu>
2019-11-19 13:28:14 -0700

    Add version config

--- xpersist/__init__.py
+++ xpersist/__init__.py
@@ -1,3 +1,11 @@
-settings = {'cache_dir': 'xpersist_cache'}
+from pkg_resources import DistributionNotFound, get_distribution
 
-from .core import *
+from .core import *  # noqa: F403, F401
+
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
+
+settings = {'cache_dir': 'xpersist_cache'}
